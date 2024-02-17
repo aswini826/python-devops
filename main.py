5ed5ce26b47b392141f4e1b8d676b2fee14d443d
ASWINI A <aswinidhas556@gmail.com>
2024-02-17 23:49:15 +0530

    commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello aswini"}
+
+@app.get("/post")
+def get_post():
+    return {"message": "This is your posts"}
+
+@app.post("/create")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new-post": f"Movie: {payload['Movie']} Genre: {payload['Genre']} Rating: {payload['Rating']}"}
