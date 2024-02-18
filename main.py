f2a8d1f9e9621828b9b20f44f732a4edc6964b9b
ASWINI A <aswinidhas556@gmail.com>
2024-02-18 22:21:55 +0530

    commit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from random import randrange
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    Movie: str
+    Genre: str
+    Rating: Optional[float] = None 
+    Published: bool = True
+
+
+new_data = [{"title": "Favorite Movie", "content": "Cruella", "id": 1}, {"title": "Favorite Food", "content": "Briyani", "id": 2}]
+
 
 @app.get("/")
 async def root():
@@ -9,9 +22,11 @@ async def root():
 
 @app.get("/post")
 def get_post():
-    return {"message": "This is your posts"}
+    return {"data": new_data}
 
 @app.post("/create")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"new-post": f"Movie: {payload['Movie']} Genre: {payload['Genre']} Rating: {payload['Rating']}"}
+def create_post(payload: Post):
+    post_dict = payload.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    new_data.append(post_dict)
+    return {"data": post_dict}
