449acb966250b1a77368205b85576fee1d91883b
ASWINI A <aswinidhas556@gmail.com>
2024-02-22 22:27:32 +0530

    commit

--- app/main.py
+++ app/main.py
@@ -1,30 +1,36 @@
 from random import randrange
+import time
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 import psycopg2
-from psycopg2.extras import ReaDictCursor
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
 class Post(BaseModel):
-    Movie: str
-    Genre: str
-    Published: bool = True
+    movie: str
+    genre: str
+    published: bool = True
     
 
 class Update(BaseModel):
     title: str
     content: str
 
-try:
-    conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='ashu', cursor_factory=ReaDictCursor)
-    cursor = conn.cursor()
-    print("Database connection was successfully")
-except Exception as error:
-    print("Connection of database failed")
-    print("Error :", error)
+while True:
+
+    try:
+       conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='ashu', cursor_factory=RealDictCursor)
+       cursor = conn.cursor()
+       print("Database connection was successfully")
+       break
+
+    except Exception as error:
+       print("Connection of database failed")
+       print("Error :", error)
+       time.sleep(2)
 
 new_data = [{"title": "Favorite Movie", "content": "Cruella", "id": 1}, {"title": "Favorite Food", "content": "Briyani", "id": 2}]
 
@@ -44,40 +50,48 @@ async def root():
 
 @app.get("/post")
 def get_post():
-    return {"data": new_data}
+    cursor.execute("""SELECT * FROM post """)
+    database = cursor.fetchall()
+    return {"data": database}
 
 @app.post("/create", status_code = status.HTTP_201_CREATED)
 def create_post(payload: Post):
-    post_dict = payload.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    new_data.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO "post" (movie, genre, published) VALUES (%s, %s, %s) RETURNING * """,
+                    (payload.movie, payload.genre, payload.published))
+    create_database = cursor.fetchone()
+    conn.commit()
+    return {"data": create_database}
 
 @app.get("/post/{id}")
 def get_idpost(id: int, response: Response):
-    find = find_id(id)
-    if not find:
+    cursor.execute("""SELECT * FROM post WHERE id = %s""", (str(id)))
+    data = cursor.fetchone()
+    print(data)
+
+    if not data:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail =f'Post with this id {id} is not available')
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'message': f'Post with this id {id} is not available'}
-    print(find)
-    return {"data": find}
+    return {"data": data}
 
 
 @app.delete("/post/{id}")
 def delete_post(id: int):
-    index = find_index(id)
-    new_data.pop(index)
-    return {'message': 'Your data deleted successfully'}
+    cursor.execute("""DELETE FROM post WHERE id = %s RETURNING * """, (str(id)))
+    delete_data = cursor.fetchone()
+    conn.commit()
+    print(delete_data)
+    if delete_data == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'Post with this id {id} is not available')
+    return Response (status_code = status.HTTP_204_NO_CONTENT)
+
+
 
 @app.put("/update/{id}")
-def update_post(id: int, posts: Update):
-    print(posts)
-    index = find_index(id)
-    if index == None:
-        raise HTTPException(status_code = status.HTTP_201_CREATED, detail=f"Post with the id {id} does not exist")
-    
-    post_dict = posts.dict()
-    post_dict['id'] = id
-    new_data[index] = post_dict
-    return {"data": post_dict}
+def update_post(id: int, posts: Post):
+    cursor.execute("""UPDATE post SET movie = %s, genre= %s, published=%s WHERE id =%s RETURNING * """, (posts.movie, posts.genre, posts.published, str(id)))
+    update_data = cursor.fetchone()
+    conn.commit()
+
+    if update_data == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"Post with the id {id} does not exist")
+
+    return {"data": update_data}
