90ed8908e45e2312973df334d86b50c43a547569
ashu <aswinidhas556@gmail.com>
2024-05-28 15:19:11 +0530

    Create auth.py file

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from . import models,schemas,utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.functions import mode
-from .router import post,user
+from .router import post,user, auth
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -52,5 +52,7 @@ async def root():
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
+
 
 
--- app/router/auth.py
+++ app/router/auth.py
@@ -1,3 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
+from sqlalchemy.orm import Session
+from .. import database, schemas, models, utils
 
 router = APIRouter(tags=['Authentication'])
+
+@router.post('/login')
+def login(user_credantials: schemas.UserLogin, Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credantials.email).first() # type: ignore
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+    
+    if not utils.verify(user_credantials.password, user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
