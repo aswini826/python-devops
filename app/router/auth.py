bd8de3670a05badfd653362792928a5375fed5eb
ashu <aswinidhas556@gmail.com>
2024-06-18 20:05:56 +0530

    create and verify login api

--- app/router/auth.py
+++ app/router/auth.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credantials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credantials.email).first() # type: ignore
+def login(user_credantials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credantials.username).first() # type: ignore
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
     
     if not utils.verify(user_credantials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
     
-    return {"token" : "example token"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    
+    return {"access_token" : access_token, "token_type": "bearer"}
